8a985e4b8cf490f6413e8e936cfa0fc3864ab7df
Najoule Morrris <najoulemorrris@Najoules-Mac-mini.local>
2026-06-24 20:14:16 -0400

    Select counter elements

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 let count = 0;
+
+const counterDisplay = document.getElementById("count");
+const increaseBtn = document.getElementById("increase-btn");
+const decreaseBtn = document.getElementById("decrease-btn");
