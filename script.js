94e8f14735c1aa8a962db56567479145d58b344b
Najoule Morrris <najoulemorrris@Najoules-Mac-mini.local>
2026-06-24 20:43:13 -0400

    Add IDs to elements

--- script.js
+++ script.js
@@ -3,3 +3,10 @@ let count = 0;
 const counterDisplay = document.getElementById("count");
 const increaseBtn = document.getElementById("increase-btn");
 const decreaseBtn = document.getElementById("decrease-btn");
+
+
+increaseBtn.addEventListener("click", function () {
+    count++;
+    counterDisplay.textContent = count;
+    
+})
